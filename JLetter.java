15da527e2ecf437353f599f29506e68b90651505
swoomp <111590660+swoomp@users.noreply.github.com>
2022-08-19 12:41:49 -0400

    Add files via upload

---
+++ JLetter.java
@@ -0,0 +1,27 @@
+/**
+ * A program that prints a large block Letter J composed of the characters JAVA.
+ *
+ * CPSC 1210 Project 1: JLetter
+ * Kenneth Will
+ * 8/19/2022
+ */
+public class JLetter
+{
+   /**
+    * Prints a block letter J composed of the characters JAVA.
+    * @param args Command line arguments - not used.
+    */
+   public static void main(String[]args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("      JAVA  ");
+      System.out.println("J     JAVA  ");
+      System.out.println("JA    JAVA  ");
+      System.out.println(" JAVAJAVA  ");
+      System.out.println("  JAVAJA   ");
+   }
+}
---
+++ MyLifeGoals.java
@@ -0,0 +1,31 @@
+/**
+ * Simple program that prints my life goals.
+ *
+ * CPSC 1210: Project 1: MyLifeGoals
+ * Kenneth Will
+ * 8/19/2022
+ */
+public class MyLifeGoals
+{
+  /** 
+   * prints my life goals in the short-term, medium-term, and long-term.
+   * @param args Command line arguments - not used. 
+   */
+   public static void main(String[]args)
+   {
+      System.out.println("Kenneth Will");
+      System.out.println();
+      System.out.println("In the short-term, my goal is to remain positive "
+         + "and vigilant in both my current workplace, "
+         + "and in my studies on a day to day basis.");
+      System.out.println("In the medium-term, my goal is to graduate from the "
+         + "Auburn Computer Science program with a 3.6 GPA or above. "
+         + "Throughout the program I will look to discover the specific "
+         + "field of computer science I am most interest in.");  
+      System.out.println("In the long-term I have two goals. " 
+         + "The first and primary goal, is to start a computer science " 
+         + "career that is challenging and fulfilling in my everyday life. "
+         + "The secondary goal, is to purchase the small business that "
+         + "I co-opperate so that I can advance my career in business."); 
+   }
+}
